73dbbfc74cecb90f1c6eb19b62f5c0dcfc8f7f20
maxliaops <maxliaops@gmail.com>
2016-04-24 23:15:06 +0800

    ZhihuDaily: get latest news

--- ZhihuDaily/ZhihuDaily/src/com/rainsong/zhihudaily/MainActivity.java
+++ ZhihuDaily/ZhihuDaily/src/com/rainsong/zhihudaily/MainActivity.java
@@ -1,15 +1,38 @@
 package com.rainsong.zhihudaily;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+
+    public static final String URL_LATEST = "http://news-at.zhihu.com/api/4/news/latest";
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+        new GetLatestNewsTask(mContext)
+                .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     @Override
@@ -17,6 +40,61 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private class GetLatestNewsTask extends AsyncTask<String, Void, String> {
+        Context mContext;
+
+        public GetLatestNewsTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String targetUrl = URL_LATEST;
+
+            ArrayList<NameValuePair> paramList = new ArrayList<NameValuePair>();
+
+            for (int i = 0; i < paramList.size(); i++) {
+                NameValuePair nowPair = paramList.get(i);
+                String value = nowPair.getValue();
+                try {
+                    value = URLEncoder.encode(value, "UTF-8");
+                } catch (Exception e) {
+                }
+                if (i == 0) {
+                    targetUrl += ("?" + nowPair.getName() + "=" + value);
+                } else {
+                    targetUrl += ("&" + nowPair.getName() + "=" + value);
+                }
+            }
+
+            Log.d(TAG, "doInBackground(): targetUrl=" + targetUrl);
+            HttpGet httpRequest = new HttpGet(targetUrl);
+            try {
+
+                HttpClient httpClient = new DefaultHttpClient();
+
+                HttpResponse httpResponse = httpClient.execute(httpRequest);
+
+                if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                    String strResult = EntityUtils.toString(httpResponse
+                            .getEntity());
+                    return strResult;
+                } else {
+                    return null;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(TAG, "onPostExecute(): result: " + result);
+        }
     }
 
 }
