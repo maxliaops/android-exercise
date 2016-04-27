80c6cdc4ec3c4b45943393d680619d1afcbafa8c
Max Liao <maxliaops@gmail.com>
2016-04-27 10:23:08 +0800

    ZhihuDaily: display image

--- ZhihuDaily/ZhihuDaily/src/com/rainsong/zhihudaily/NewsAdapter.java
+++ ZhihuDaily/ZhihuDaily/src/com/rainsong/zhihudaily/NewsAdapter.java
@@ -11,6 +11,8 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.rainsong.zhihudaily.NewsListEntity.NewsEntity;
 
 public class NewsAdapter extends BaseAdapter {
@@ -18,12 +20,22 @@ public class NewsAdapter extends BaseAdapter {
     protected Context mContext;
     protected LayoutInflater mInflater;
     protected List<NewsEntity> mDataList;
+    private ImageLoader mImageLoader;
+    private DisplayImageOptions mOptions;
 
     public NewsAdapter(Context context) {
         mContext = context;
         mInflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mDataList = new ArrayList<NewsEntity>();
+        mImageLoader = ImageLoader.getInstance();
+        mOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.drawable.image_small_default) // 设置图片下载期间显示的图片
+                .showImageForEmptyUri(R.drawable.image_small_default) // 设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.drawable.image_small_default) // 设置图片加载或解码过程中发生错误显示的图片
+                .cacheInMemory(true) // 设置下载的图片是否缓存在内存中
+                .cacheOnDisk(true) // 设置下载的图片是否缓存在SD卡中
+                .build(); // 构建完成
 
     }
 
@@ -56,12 +68,15 @@ public class NewsAdapter extends BaseAdapter {
             holder = new ViewHolder();
             holder.title = (TextView) convertView
                     .findViewById(R.id.list_item_title);
+            holder.image = (ImageView) convertView
+                    .findViewById(R.id.list_item_image);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
         NewsEntity item = mDataList.get(position);
         holder.title.setText(item.title);
+        mImageLoader.displayImage(item.images.get(0), holder.image, mOptions);
 
         return convertView;
     }
