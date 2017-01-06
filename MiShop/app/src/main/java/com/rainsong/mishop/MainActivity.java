b9cc93b1b178b3858f6049e46400d39b8a0063c2
Max Liao <maxliaops@gmail.com>
2017-01-06 11:28:07 +0800

    MiShop: implement bottom navigation bar

--- MiShop/app/src/main/java/com/rainsong/mishop/MainActivity.java
+++ MiShop/app/src/main/java/com/rainsong/mishop/MainActivity.java
@@ -1,13 +1,53 @@
 package com.rainsong.mishop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar
+        .OnTabSelectedListener {
+    BottomNavigationBar bottomNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        bottomNavigationBar.setTabSelectedListener(this);
+        bottomNavigationBar.clearAll();
+        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.icon_main_home_selected, R.string.home)
+                        .setInactiveIconResource(R.drawable.icon_main_home_normal)
+                        .setActiveColorResource(R.color.orange))
+                .addItem(new BottomNavigationItem(R.drawable.icon_main_category_selected, R.string.category)
+                        .setInactiveIconResource(R.drawable.icon_main_category_normal)
+                        .setActiveColorResource(R.color.orange))
+                .addItem(new BottomNavigationItem(R.drawable.icon_main_discover_selected, R.string.discover)
+                        .setInactiveIconResource(R.drawable.icon_main_discover_normal)
+                        .setActiveColorResource(R.color.orange))
+                .addItem(new BottomNavigationItem(R.drawable.icon_main_mine_selected, R.string.mine)
+                        .setInactiveIconResource(R.drawable.icon_main_mine_normal)
+                        .setActiveColorResource(R.color.orange))
+                .initialise();
+    }
+
+    @Override
+    public void onTabSelected(int position) {
+
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
+
     }
 }
