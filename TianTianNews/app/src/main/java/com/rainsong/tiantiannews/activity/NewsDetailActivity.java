0dda40db6ec32ab7afd96918fe9c8ac5cfcdb5ce
Max Liao <maxliaops@gmail.com>
2017-02-10 16:15:53 +0800

    TianTianNews: add ActionBar

--- TianTianNews/app/src/main/java/com/rainsong/tiantiannews/activity/NewsDetailActivity.java
+++ TianTianNews/app/src/main/java/com/rainsong/tiantiannews/activity/NewsDetailActivity.java
@@ -1,8 +1,9 @@
 package com.rainsong.tiantiannews.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
 import com.rainsong.tiantiannews.R;
@@ -12,7 +13,7 @@ import com.rainsong.tiantiannews.entity.NewsListEntity.Result.NewsEntity;
  * Created by maxliaops on 17-1-14.
  */
 
-public class NewsDetailActivity extends Activity {
+public class NewsDetailActivity extends AppCompatActivity {
     private NewsEntity mNewsEntity;
     private WebView mWebView;
 
@@ -21,10 +22,21 @@ public class NewsDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         Intent intent = getIntent();
-        mNewsEntity = (NewsEntity)intent.getSerializableExtra("newsEntity");
+        mNewsEntity = (NewsEntity) intent.getSerializableExtra("newsEntity");
 
         mWebView = (WebView) findViewById(R.id.webview);
         mWebView.loadUrl(mNewsEntity.url);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
     }
 }
